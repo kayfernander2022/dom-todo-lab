aab128354c75833ed7522f29fcdd7a30e2b5e051
kayfernander2022 <kayfernander@hotmail.com>
2022-08-30 23:49:19 -0400

    complete the lab

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
-const inputElement = document.getElementById("#list") // id?
-const buttonElement = document.getElementById("#submit-button") //id?
-const ulElement = document.getElementById("#todo-List") //id?
+const inputElement = document.getElementById("input-field") 
+const buttonElement = document.getElementById("submit-button") 
+const ulElement = document.getElementById("todo-List")
 
-console.log(inputElement);
+console.log(inputElement.value);
+
+buttonElement.addEventListener('click', function(){
+  const li = document.createElement('li');
+  li.textContent = inputElement.value;
+  console.log(li);
+  ulElement.appendChild(li);
+
+  inputElement.value = '';
+});
+  
+console.log()
