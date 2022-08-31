7c7f4f58f773a3bf5f3fd05fd6914cdfd1497165
kayfernander2022 <kayfernander@hotmail.com>
2022-08-30 22:47:36 -0400

    cached elements

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const inputElement = document.getElementById("#list") // id?
+const buttonElement = document.getElementById("#submit-button") //id?
+const ulElement = document.getElementById("#todo-List") //id?
+
+console.log(inputElement);
